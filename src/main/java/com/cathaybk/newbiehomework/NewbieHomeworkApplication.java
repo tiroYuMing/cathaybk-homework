8b34f2b4680d64c8f7746e9e6df3a7d08d27843c
manson.su <chewen51@gmail.com>
2024-07-12 00:57:21 +0800

    add: 新增專案架構

---
+++ src/main/java/com/cathaybk/newbiehomework/NewbieHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.cathaybk.newbiehomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbieHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewbieHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cathaybk/newbiehomework/NewbieHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cathaybk.newbiehomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewbieHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
